442799ecaffad88363aa36b79a0e723c447960de
Abhilash Pitla <pitlaabhilash6@gmail.com>
2025-09-03 23:35:57 +0530

    Strings

---
+++ Strings/stringconcatination.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class stringconcatination {
+    public static void main(String[] args) {
+        
+    String firstname="Abhilash";
+    String lastname=" Pitla";
+    String fullname=firstname+lastname;
+    System.out.println("My full name is : "+fullname);
+    }
+}
